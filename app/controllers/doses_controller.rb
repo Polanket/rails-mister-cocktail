5064be85f5078db54e2ec5b2162e2fdc97d33733
Juan Polanco <juanmpt@outlook.com>
2019-07-11 17:47:50 +0200

    created views for cocktail and dose form

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    cocktail = Cocktail.find(params[:cocktail_id])
+    ingredient = Ingredient.find(params[:dose][:ingredient_id])
+
+    @dose.cocktail = cocktail
+    @dose.ingredient = ingredient
+
+    @dose.save
+
+    redirect_to cocktails_path
+  end
+
+  def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Clearing database'
+
+Ingredient.delete_all
+Cocktail.delete_all
+
+puts 'Calling json api'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+get_json = open(url).read
+ingredients_hash = JSON.parse(get_json)
+
+puts 'Injecting ingredients'
+
+ingredients_hash['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+  # Ingredient.create(name: ingredient)
+end
+
+puts "Injecting cocktails"
+
+20.times do
+  Cocktail.create(name: Faker::Cannabis.strain)
+end
+
+puts 'Seed succesful'
