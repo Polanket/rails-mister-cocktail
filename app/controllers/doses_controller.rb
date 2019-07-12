0cf28c41a8476da9851218950f833e7196b891a4
Juan Polanco <juanmpt@outlook.com>
2019-07-12 14:52:05 +0200

    added h1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,16 +2,16 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
 
-    cocktail = Cocktail.find(params[:cocktail_id])
-    ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
 
-    @dose.cocktail = cocktail
-    @dose.ingredient = ingredient
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
 
     if @dose.save
-      redirect_to cocktail_path(cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/show"
+      render 'cocktails#show'
     end
   end
 
