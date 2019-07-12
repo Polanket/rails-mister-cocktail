47d979269838d74f8ae34fd24a5f5dc3977a48a5
Juan Polanco <juanmpt@outlook.com>
2019-07-12 15:19:02 +0200

    Fixed simple_form error handling

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,15 +3,13 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
 
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
 
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
 
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render 'cocktails#show'
+      render 'cocktails/show'
     end
   end
 
@@ -21,6 +19,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
